2150be3f62cc1d546e9255af174e12efa6adb4b4
Suriyha2808 <suriyha2808@gmail.com>
2026-04-08 00:25:51 +0530

    Add: find-duplicate-integer - submission-9

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-9.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        // Phase 1: Find the intersection point
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        
+        // Phase 2: Find the entrance to the cycle (the duplicate)
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
+    }
+}
