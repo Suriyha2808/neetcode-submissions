26b9e15a2f83b73fca552b6778fba5bba37a4f99
Suriyha2808 <suriyha2808@gmail.com>
2026-04-07 22:03:54 +0530

    Add: find-duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-6.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            // Step 1: Get the absolute value FIRST, then find the index
+            int index = Math.abs(nums[i]) - 1;
+            
+            // Step 2: Check if the "house" at this index is already marked
+            if (nums[index] < 0) 
+            {
+                return Math.abs(nums[i]); // Return the positive version of the number
+            }
+            
+            // Step 3: Mark the "house" as visited
+            nums[index] *= -1;
+        }
+        return -1;
+    }
+}
