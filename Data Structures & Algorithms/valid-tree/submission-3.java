d3d59b5bc7193606c1f7f677f288426916ea70aa
Suriyha2808 <suriyha2808@gmail.com>
2026-07-17 23:54:59 +0530

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        if(edges.length!=n-1)
+        {
+            return false;
+        }
+
+        List<List<Integer>> list = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        int l=0;
+        boolean[] visit = new boolean[n];
+       
+        for (int i = 0; i < n; i++) 
+        {
+            list.add(new ArrayList<>());
+        }
+        for(int i=0;i<edges.length;i++)
+        {
+            list.get(edges[i][0]).add(edges[i][1]);
+            list.get(edges[i][1]).add(edges[i][0]);
+        }
+        
+        visit[0]=true;
+        queue.add(0);
+        l++;
+
+        while(!queue.isEmpty())
+        {
+            int k=queue.poll();
+            for(int n1 : list.get(k))
+            {
+                if(!visit[n1])
+                {
+                    visit[n1]=true;
+                    queue.add(n1);
+                    l++;
+                }
+            }
+        }
+
+        return l==n;
+    }
+}
